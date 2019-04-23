d089fc89cdc9d57b1295c940c38b5790990145f9
Susan Tan <ssusantan9@gmail.com>
2019-04-23 15:22:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,9 +6,11 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    puts params
-    erb :reversed
-  end
+  original_string = params["string"]
+  reversed_string = original_string.reverse
+ 
+  erb :reversed
+end
 
   get '/friends' do
     get '/friends' do
